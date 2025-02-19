f065dcadaddf8eea2d0b7e3558be1e3297eac0f6
Oliver Klein <oliver@otk.studio>
2025-02-19 17:41:42 +0100

    feat(content): update copy, add patreon, obscura & op-n, fix playback issue

--- transcode.sh
+++ transcode.sh
@@ -9,6 +9,18 @@ fi
 # Get the filename without the file extension
 FILENAME=${1%%.*}
 
-ffmpeg -i $1 -c:v libx264 -crf 24 -vf scale=$2:-2 -movflags faststart -an $3/${FILENAME}_h264.mp4
-ffmpeg -i $1 -c:v libvpx-vp9 -crf 36 -deadline best -vf scale=$2:-2 -an $3/${FILENAME}.webm
-ffmpeg -i $1 -c:v libx265 -crf 28 -preset veryslow -vf scale=$2:-2 -tag:v hvc1 -movflags faststart -an $3/${FILENAME}_h265.mp4
+# ffmpeg -i $1 -c:v libx264 -crf 24 -vf scale=$2:-2 -movflags faststart -an $3/${FILENAME}_h264.mp4
+# ffmpeg -i $1 -c:v libvpx-vp9 -crf 36 -deadline best -vf scale=$2:-2 -an $3/${FILENAME}.webm
+# ffmpeg -i $1 -c:v libx265 -crf 28 -preset veryslow -vf scale=$2:-2 -tag:v hvc1 -movflags faststart -an $3/${FILENAME}_h265.mp4
+
+# ffmpeg -i $1 -c:v libx264 -crf 24 -vf scale=$2:-2 -movflags faststart -an -preset fast $3/${FILENAME}_h264.mp4
+# ffmpeg -i $1 -c:v libvpx-vp9 -crf 36 -deadline good -vf scale=$2:-2 -an $3/${FILENAME}.webm
+# ffmpeg -i $1 -c:v libx265 -crf 24 -preset fast -vf scale=$2:-2 -tag:v hvc1 -movflags faststart -an $3/${FILENAME}_h265.mp4
+
+# ffmpeg -i $1 -c:v libx264 -crf 24 -vf "scale=$2:-2,fps=30" -movflags faststart -an $3/${FILENAME}_h264.mp4
+# ffmpeg -i $1 -c:v libvpx-vp9 -crf 36 -b:v 0 -b:a 128k -deadline good -cpu-used 4 -vf "scale=$2:-2,fps=30" -an $3/${FILENAME}.webm
+# ffmpeg -i $1 -c:v libx265 -crf 28 -preset medium -vf "scale=$2:-2,fps=30" -tag:v hvc1 -movflags faststart -an $3/${FILENAME}_h265.mp4
+
+ffmpeg -i $1 -c:v libx264 -crf 24 -vf "scale=$2:-2,fps=30" -movflags faststart -an $3/${FILENAME}_h264.mp4
+ffmpeg -i $1 -c:v libvpx-vp9 -crf 36 -deadline best -vf "scale=$2:-2,fps=30" -an $3/${FILENAME}.webm
+ffmpeg -i $1 -c:v libx265 -crf 28 -preset veryslow -vf "scale=$2:-2,fps=30" -tag:v hvc1 -movflags faststart -an $3/${FILENAME}_h265.mp4
