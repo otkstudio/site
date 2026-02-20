36b209d3d12107da670b4159340a937475448056
Oliver Klein <oliver@otk.studio>
2026-02-20 22:13:04 +0100

    remove addition routes

--- transcode.sh
+++ transcode.sh
@@ -21,6 +21,6 @@ FILENAME=${1%%.*}
 # ffmpeg -i $1 -c:v libvpx-vp9 -crf 36 -b:v 0 -b:a 128k -deadline good -cpu-used 4 -vf "scale=$2:-2,fps=30" -an $3/${FILENAME}.webm
 # ffmpeg -i $1 -c:v libx265 -crf 28 -preset medium -vf "scale=$2:-2,fps=30" -tag:v hvc1 -movflags faststart -an $3/${FILENAME}_h265.mp4
 
-ffmpeg -i $1 -c:v libx264 -crf 24 -vf "scale=$2:-2,fps=30" -movflags faststart -an $3/${FILENAME}_h264.mp4
-ffmpeg -i $1 -c:v libvpx-vp9 -crf 36 -deadline best -vf "scale=$2:-2,fps=30" -an $3/${FILENAME}.webm
-ffmpeg -i $1 -c:v libx265 -crf 28 -preset veryslow -vf "scale=$2:-2,fps=30" -tag:v hvc1 -movflags faststart -an $3/${FILENAME}_h265.mp4
+ffmpeg -i $1 -c:v libx264 -crf 19 -vf "scale=$2:-2" -fps_mode cfr -movflags faststart -an $3/${FILENAME}_h264.mp4
+ffmpeg -i $1 -c:v libvpx-vp9 -crf 29 -deadline best -vf "scale=$2:-2" -fps_mode cfr -an $3/${FILENAME}.webm
+ffmpeg -i $1 -c:v libx265 -crf 22 -preset veryslow -vf "scale=$2:-2" -fps_mode cfr -tag:v hvc1 -movflags faststart -an $3/${FILENAME}_h265.mp4
